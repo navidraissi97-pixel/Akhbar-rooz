c2f2d940e1ddc3799040d75e9721a017958766ed
navidraissi97-pixel <navid.raissi97@gmail.com>
2026-08-28 08:15:41 +0330

    Update news.js

--- news.js
+++ news.js
@@ -1,1 +1,8 @@
-alert("تست نهایی");
+const newsList = document.querySelector("#newsList");
+
+newsList.innerHTML = 
+  <div class="news-item">
+    <h2>خبر آزمایشی اخبار روز</h2>
+    <p>اگر این خبر را می‌بینی، سیستم نمایش اخبار درست کار می‌کند.</p>
+  </div>
+;
