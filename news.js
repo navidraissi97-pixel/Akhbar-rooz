5a27e729ca11899998436e2e338409bc6499d9cf
navidraissi97-pixel <navid.raissi97@gmail.com>
2026-08-28 07:34:08 +0330

    Update news.js

--- news.js
+++ news.js
@@ -1,18 +1,22 @@
 async function loadNews() {
-  const dataBox = document.querySelector(".data");
+  const newsList = document.querySelector("#newsList");
 
-  if (!dataBox) return;
+  if (!newsList) return;
 
-  dataBox.innerHTML = "<p>در حال تست...</p>";
+  newsList.innerHTML = "<p>⏳ در حال دریافت اخبار...</p>";
 
   try {
     const response = await fetch(
-      "https://jsonplaceholder.typicode.com/posts?_limit=5"
+      "https://jsonplaceholder.typicode.com/posts?_limit=10"
     );
+
+    if (!response.ok) {
+      throw new Error("خطا در اتصال");
+    }
 
     const data = await response.json();
 
-    dataBox.innerHTML = "";
+    newsList.innerHTML = "";
 
     data.forEach(news => {
       const article = document.createElement("div");
@@ -23,12 +27,13 @@ async function loadNews() {
         <p>${news.body}</p>
       ;
 
-      dataBox.appendChild(article);
+      newsList.appendChild(article);
     });
 
   } catch (error) {
     console.error(error);
-    dataBox.innerHTML = "<p>اتصال برقرار نشد.</p>";
+    newsList.innerHTML =
+      "<p>❌ خطا در دریافت اخبار</p>";
   }
 }
 
