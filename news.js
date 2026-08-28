a9b8f8f0798dc169ad54ce5df047d9e371547072
navidraissi97-pixel <navid.raissi97@gmail.com>
2026-08-28 08:50:38 +0330

    Update news.js

--- news.js
+++ news.js
@@ -1,5 +1,11 @@
 const newsList = document.getElementById("newsList");
 
 if (newsList) {
-  newsList.innerHTML = "<h2>تست اخبار روز ✅</h2>";
+  newsList.innerHTML = 
+    <div class="news-item">
+      <h2>خبر آزمایشی</h2>
+      <p>اگر این متن را می‌بینی، سیستم نمایش اخبار آماده است.</p>
+      <a href="#" target="_blank">ادامه خبر</a>
+    </div>
+  ;
 }
